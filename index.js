c14a2b721ac33aab4ba1ba44586f4ebf13b685cd
Tressie Muthuri <tressiemuthuri@gmail.com>
2022-08-08 01:10:58 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,26 @@
+const name = "Aki";
+
+function introduction (name) {
+    if (name === "Aki"){
+    return `Hi, my name is ${name}.`
+} else if (name === "Samip"){
+    return `Hi, my name is ${name}.`;
+}
+}
+console.log(name);
+
+const language = "Ember.js";
+
+function introductionWithLanguage (name, language){
+    if (name === "Aki"){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+} else if (name === "Samip", language === "React"){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+};
+console.log (name, language);
+
+function introductionWithLanguageOptional (name = "Gracie", language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+console.log(name, language);
